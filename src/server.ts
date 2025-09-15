f84a5420d6c0ce7ccc920749f39b8e22c4c6a322
sionzee <jan@sionzee.cz>
2025-09-16 01:26:37 +0200

    feat: support for --temp-database flag

--- src/server.ts
+++ src/server.ts
@@ -3,30 +3,37 @@ import { CORSPlugin } from "@orpc/server/plugins";
 import { router } from "./contract/router.ts";
 import "dotenv/config";
 import type { IncomingHttpHeaders } from "node:http";
-import { drizzle } from "drizzle-orm/bun-sql";
+import {type BunSQLDatabase, drizzle} from "drizzle-orm/bun-sql";
 import { createTestDatabase } from "./contract/shared/test-utils.ts";
 import { relations } from "./db/relations.ts";
 import type { DbUser } from "./db/schema.ts";
 import * as schema from "./db/schema.ts";
 
-if (process.env.DATABASE_URL === undefined)
+// env variable USE_TEMP_DATABASE can be set to "true" or switch --temp-database to use a temporary in-memory database
+const isTempDatabase = process.env.USE_TEMP_DATABASE === "true" || Bun.argv.includes("--temp-database");
+
+if (process.env.DATABASE_URL === undefined && !isTempDatabase)
 	throw new Error("DATABASE_URL environment variable is not set. Please set it to your database URL.");
 
-const client = new Bun.SQL(process.env.DATABASE_URL, {
-	max: 20, // Maximum 20 concurrent connections
-	idleTimeout: 30, // Close idle connections after 30s
-	maxLifetime: 3600, // Max connection lifetime 1 hour
-	connectionTimeout: 10, // Connection timeout 10s
-});
+let client: Bun.SQL | undefined
+let db: BunSQLDatabase<typeof schema, typeof relations>
 
-const db =
-	process.env.USE_TEMP_DATABASE === "true"
-		? await createTestDatabase()
-		: drizzle({
-				client,
-				schema,
-				relations,
-			});
+if(isTempDatabase) {
+    console.warn("⚠️ WARNING: Using a temporary database. All data will be lost when the server stops.");
+    db = await createTestDatabase()
+} else {
+    client = new Bun.SQL(process.env.DATABASE_URL as string, {
+        max: 20, // Maximum 20 concurrent connections
+        idleTimeout: 30, // Close idle connections after 30s
+        maxLifetime: 3600, // Max connection lifetime 1 hour
+        connectionTimeout: 10, // Connection timeout 10s
+    });
+    db = drizzle({
+        client: client as Bun.SQL,
+        schema,
+        relations,
+    })
+}
 
 // To detect if we are connected to the database, if not it will throw an error
 await db.execute("SELECT 1");
@@ -55,6 +62,8 @@ console.log(`🚀 Server running at http://${server.hostname}:${server.port}`);
 // Graceful shutdown
 process.on("SIGTERM", async () => {
 	console.log("SIGTERM signal received: closing HTTP server");
-	await client.end();
+    if(client) {
+        await client.end();
+    }
 	console.log("Database pool closed");
 });
